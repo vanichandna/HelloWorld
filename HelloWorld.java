fd655353474d77ef8aed01a13c3600271bc95388
vanichandna <71704707+vanichandna@users.noreply.github.com>
2022-08-02 16:36:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  public static void main(String [] s)
+  {
+      System.out.println("Hello oracle");
+      for (int i=0;i<10;i++)
+	System.out.println("value of i is "+i);
+  }
+}
